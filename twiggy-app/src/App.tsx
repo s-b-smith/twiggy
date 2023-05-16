bc041c7a227d0eef361401fa744c1801c2e6fa75
Skylar Smith <s.b.smith97@gmail.com>
2023-05-15 20:47:42 -0500

    Initial nav drawer with app bar

---
+++ twiggy-app/src/App.tsx
@@ -0,0 +1,7 @@
+import NavDrawer from './components/NavDrawer';
+
+const App = () => {
+  return <NavDrawer />;
+};
+
+export default App;
---
+++ twiggy-app/src/components/NavDrawer.tsx
@@ -0,0 +1,178 @@
+import {
+  AppBar as MuiAppBar,
+  AppBarProps as MuiAppBarProps,
+  CSSObject,
+  Divider,
+  Drawer as MuiDrawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Theme,
+  styled,
+  useTheme,
+  Toolbar,
+  Typography,
+  Box,
+  Stack
+} from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import { ChevronLeft, ChevronRight, Inbox, Mail, Menu } from '@mui/icons-material';
+import { useState } from 'react';
+
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen
+  }),
+  overflowX: 'hidden'
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`
+  }
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: prop => prop !== 'open' })(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  ...(open && {
+    ...openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme)
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme)
+  })
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: prop => prop !== 'open'
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}));
+
+const NavDrawer = () => {
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => setOpen(true);
+  const handleDrawerClose = () => setOpen(false);
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          {/* <Grid container spacing={3}>
+            <Grid xs={3} justifyContent="left">
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                sx={{ mr: 5, ...(open && { display: 'none' }) }}
+              >
+                <Menu />
+              </IconButton>
+            </Grid>
+            <Grid xs={9} justifyContent="left">
+              <Typography variant="h6" noWrap component="div" alignContent="center">
+                Twiggy
+              </Typography>
+            </Grid>
+          </Grid> */}
+          {/* <Box display="flex" justifyContent="center" alignItems="center">
+            <Typography variant="h6" noWrap component="div" alignContent="center">
+              Twiggy
+            </Typography>
+          </Box> */}
+          {/* <Stack direction="row" alignItems="center" spacing={2}> */}
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            sx={{ mr: 5, ...(open && { display: 'none' }) }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div" alignContent="center">
+            Twiggy
+          </Typography>
+          {/* </Stack> */}
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" anchor="left" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRight /> : <ChevronLeft />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center'
+                  }}
+                >
+                  {index % 2 === 0 ? <Inbox /> : <Mail />}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default NavDrawer;
